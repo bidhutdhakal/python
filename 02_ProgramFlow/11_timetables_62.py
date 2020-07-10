b28e0aa15387e585ff4145c9f492c79d5a7fcbad
Dhakal, Bidhut <bidhut1996@gmail.com>
2020-07-10 00:54:06 -0400

    asf

--- 02_ProgramFlow/11_timetables_62.py
+++ 02_ProgramFlow/11_timetables_62.py
@@ -1,4 +1,9 @@
-for i in range (1, 13):
-    for j in range (1, 13):
-        print("{0} tines {1} is {2}".format(j, i, i * j))
-        print("_________________________")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+for i in range (1, 6):
+    for j in range (1, 11):
+        print("{0} times {1} is {2}".format(j, i, i * j))
+    print("_________________________")
+# Note: Last print is indented below the for j loop so it will print after complete of its iteration.
+# Note: if its on line below print then it will print in every line, while if its print on the initial of i for, it will print onlny one time.
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
